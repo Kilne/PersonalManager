9b53498e9cb444255d6f5fc4ffc60f2680656ce9
Kilne <46303813+Kilne@users.noreply.github.com>
2022-11-10 16:30:02 +0100

    Factory fails

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -2,8 +2,7 @@ package org.project;
 
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -24,16 +23,7 @@ public class Main {
             // Wipe confidential data
             argon2.wipeArray(password);
         }
-        System.out.println("Hello world!");
 
-        try (JedisPool pool = new JedisPool("localhost", 6379)) {
-            try (Jedis jedis = pool.getResource()) {
-
-                System.out.println(jedis.set("foo", "bar"));
-
-            }
-
-        }
 
     }
 }
--- src/main/java/org/project/database/factory/ClientFactory.java
+++
@@ -1,9 +0,0 @@
-package org.project.database.factory;
-
-import org.project.database.redis.RedisBasicClient;
-
-public abstract class ClientFactory {
-
-    public abstract RedisBasicClient createRedisClient(String host, int port);
-
-}
